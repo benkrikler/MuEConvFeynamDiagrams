df8bd7387bed6e0dfd0331bb32db25abca4c61a3
Ben Krikler <bek07@ic.ac.uk>
2014-12-02 12:43:51 +0000

    Initial add of feynman diagrams for mu-e conversion processes

---
+++ mk_diagrams.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+if [ $# -lt 1 ];then
+	echo ERROR: I need the name of the latex file to process
+	exit
+fi
+
+TmpFile=`mktemp`
+TmpFile2=`mktemp`
+pdflatex $@
+find . -newer "$TmpFile" -name '*.mp' -type f -print > "$TmpFile2"
+Lines="`wc -l $TmpFile2|cut -f1 -d' '`"
+if [ "$Lines" -gt 0 ];then
+	set -e
+cat "$TmpFile2" |while read line; do
+echo
+echo Run mg for: $line
+	mpost $line;
+done
+echo
+echo Run pdflatex again
+pdflatex $@
+fi
+
+PdfFile=` sed -e 's!\.[^.]*$!!' <<<$@`.pdf
+pdfcrop "$PdfFile"
+
+rm -f "$TmpFile"
+rm -f "$TmpFile2"
